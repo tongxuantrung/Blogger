731af9123199b5f5d0032cc05b4808a09c029cc9
Trung <105800385+tongxuantrung@users.noreply.github.com>
2024-04-02 22:49:22 +0700

    Create topic-header.js

---
+++ 2001/js/topic-header.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.topic');
+
+button.addEventListener('click',changeColor);
+
+function changeColor(){
+    const background = document.querySelector('header');
+    const arrayColor = ['#ffebee','#e0f2f1','#e2f1fc','#fff8e1','#eceff1','#fbe9e7'];
+    let random = arrayColor[randomColor(arrayColor)];
+    background.style.backgroundColor = random;
+    // console.log(random);
+}
+function randomColor(array){
+    return Math.floor(Math.random()*array.length);
+}
