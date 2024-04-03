506b65eb8f7b83e6ec6103abedb9eba907f50a77
Trung <105800385+tongxuantrung@users.noreply.github.com>
2024-04-03 09:47:45 +0700

    Update topic-header.js

--- 2001/js/topic-header.js
+++ 2001/js/topic-header.js
@@ -3,7 +3,7 @@ const button = document.querySelector('.topic');
 button.addEventListener('click',changeColor);
 
 function changeColor(){
-    const background = document.querySelector('header');
+    const background = document.querySelector('.mainMenu');
     const arrayColor = ['#ffebee','#e0f2f1','#e2f1fc','#fff8e1','#eceff1','#fbe9e7'];
     let random = arrayColor[randomColor(arrayColor)];
     background.style.backgroundColor = random;
