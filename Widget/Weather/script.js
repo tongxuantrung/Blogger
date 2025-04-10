f92badf0dd20dc008d8c9536b6650cdf0824313e
Trung <105800385+tongxuantrung@users.noreply.github.com>
2025-04-10 20:39:09 +0700

    Update script.js

--- Widget/Weather/script.js
+++ Widget/Weather/script.js
@@ -1,5 +1,6 @@
         function getWeather() {
-            const apiKey = 'fb63540f2dbf97d229b3c7ceddc13b53'; // Thay thế bằng API key của bạn
+            const apiKey = '3652719b430b5b7602ae27015fab39a5
+'; // Thay thế bằng API key của bạn
             const city = 'Hanoi, VN'; // Thành phố Hà Nội
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
